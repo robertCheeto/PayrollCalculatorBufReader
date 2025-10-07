5defec592f3aaaf442404563f03b854007021b88
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-07 09:50:47 -0400

    * created Employee Class for later in the project * in Main, tested using BufferedReader, FileReader to read a txt file within the project

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,23 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        // try to read text from a file first before created the payroll calc
+        try {
+            FileReader fileReader = new FileReader("src/main/resources/goldilocks.txt");
+            BufferedReader bufReader = new BufferedReader(fileReader);
+            String text;
+            while ((text = bufReader.readLine()) != null) {
+                System.out.println(text);
+            }
+            bufReader.close();
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
