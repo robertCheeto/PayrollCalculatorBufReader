e6d6548962cb9b808b25c17d78eedfdc45442034
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-07 09:55:19 -0400

    * created private variables * created Employee Constructor * created getters/setters for employee attributes * created getter for Gross Salary

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -1,4 +1,53 @@
 package com.pluralsight;
 
 public class Employee {
+    private int employeeID;
+    private String employeeName;
+    private double hoursWorked;
+    private double payRate;
+
+    public Employee(int employeeID, double payRate, double hoursWorked, String employeeName) {
+        this.employeeID = employeeID;
+        this.payRate = payRate;
+        this.hoursWorked = hoursWorked;
+        this.employeeName = employeeName;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public double getGrossSalary() {
+        double grossSalary = this.hoursWorked * this.payRate;
+        return grossSalary;
+    }
+
 }
