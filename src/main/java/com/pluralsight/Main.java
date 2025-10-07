02bafd6755c4f3c86179324259521cb5cfc019bb
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-07 14:23:45 -0400

    * i have no idea what i have done in the past hour

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,51 +1,75 @@
 package com.pluralsight;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
 
+        System.out.println("Please enter the number corresponding with the file you want to process...");
+        System.out.print("1) employees.csv OR 2) employeesExport.csv: ");
+        int userChoice = keyboard.nextInt();
+        keyboard.nextLine();
+
+        switch (userChoice) {
+            case(1):
+                processEmployeeCSV();
+                System.out.print("Would you like to rename the output of the file? (Y/N)");
+                String userInput = keyboard.nextLine().toLowerCase().trim();
+                switch (userInput) {
+                    case("y"):
+                        System.out.print("Please enter the name of the file: ");
+                        userInput = keyboard.nextLine();
+                    case("n"):
+                        break;
+                }
+                break;
+            case(2):
+                processEmployeeCSV();
+                System.out.println("hehe");
+                break;
+            default:
+                System.out.println("Please enter a proper file name");
+        } // end of switch statement
+
+        } // end of main()
+
+    public static void processEmployeeCSV() {
         Employee[] employeeList = new Employee[10];
         int count = 0;
 
         try {
             FileReader fileReader = new FileReader("src/main/resources/employees.csv");
             BufferedReader bufReader = new BufferedReader(fileReader);
+            FileWriter fileWriter = new FileWriter("src/main/resources/employeesExport.csv");
+            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
             String input;
-            bufReader.readLine();
+            bufWriter.write("id|name|gross pay\n");
 
             while ((input = bufReader.readLine()) != null) {
                 String[] parsedList = input.split("\\|");
+                if (!parsedList[0].equals("id")) {
+                    int id = Integer.parseInt(parsedList[0]);
+                    String name = parsedList[1];
+                    double hoursWorked = Double.parseDouble(parsedList[2]);
+                    double payRate = Double.parseDouble(parsedList[3]);
 
-                int id = Integer.parseInt(parsedList[0]);
-                String name = parsedList[1];
-                double  hoursWorked = Double.parseDouble(parsedList[2]);
-                double  payRate = Double.parseDouble(parsedList[3]);
+                    employeeList[count] = new Employee(id, name, hoursWorked, payRate);
+                    System.out.printf("Employee ID: %d | Employee Name: %s | Gross Pay: $%.2f\n", employeeList[count].getEmployeeID(), employeeList[count].getEmployeeName(), employeeList[count].getGrossSalary());
 
-                employeeList[count] = new Employee(id, name, hoursWorked, payRate);
-                System.out.printf("Employee ID: %d | Employee Name: %s | Gross Pay: $%.2f\n", employeeList[count].getEmployeeID(), employeeList[count].getEmployeeName(), employeeList[count].getGrossSalary());
-                count++;
+                    bufWriter.write(employeeList[count].getEmployeeID() + "|" + employeeList[count].getEmployeeName() + "|" + employeeList[count].getGrossSalary() + "\n");
+
+                    count++;
+                } // end of if statement
             } // end of while loop
             bufReader.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } // and of try and catch
-
-        try {
-            FileWriter fileWriter = new FileWriter("src/main/resources/employeesExport.csv");
-            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
-
-
-            } // end of while loop
             bufWriter.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         } // and of try and catch
+    } // end of processCSV
 
-    } // end of main()
+    }
 
-    // potentially create a method to correctly parse data from the csv into variable types
-
-}
